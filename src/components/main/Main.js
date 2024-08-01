fe481083f2584c8672c227331682a1ee9cb8baf8
halamhung <hungdeptrai111@gmail.com>
2024-08-01 14:00:19 +0700

    1/8/2024

---
+++ src/components/main/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../header/Header";
+import Footer from "../footer/Footer";
+import Product from "../product/Product";
+
+export default function Main() {
+  return (
+    <>
+      <Header />
+      <Product />
+      <Footer />
+    </>
+  );
+}
