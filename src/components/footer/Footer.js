569badeba1f9a830d3818eaf65855bb534bc54be
halamhung <hungdeptrai111@gmail.com>
2024-08-07 16:46:57 +0700

    7/8/2024

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,49 @@
 import React from "react";
-
+import { Col, Container, Row, List } from "reactstrap";
+import "./footer.css";
 export default function Footer() {
-  return <div>Footer</div>;
+  return <>
+
+    <Container className="footer">
+      <Row>
+        <Col lg={3}>
+          <List>
+            <h3>Company</h3>
+            <li>About Us</li>
+            <li>Press</li>
+            <li>Careers</li>
+            <li>Blog</li>
+          </List>
+        </Col>
+        <Col lg={3}>
+          <List>
+            <h3>Company</h3>
+            <li>About Us</li>
+            <li>Press</li>
+            <li>Careers</li>
+            <li>Blog</li>
+          </List>
+        </Col>
+        <Col lg={3}>
+          <List>
+            <h3>Company</h3>
+            <li>About Us</li>
+            <li>Press</li>
+            <li>Careers</li>
+            <li>Blog</li>
+          </List>
+        </Col>
+        <Col lg={3}>
+          <List>
+            <h3>Company</h3>
+            <li>About Us</li>
+            <li>Press</li>
+            <li>Careers</li>
+            <li>Blog</li>
+          </List>
+        </Col>
+      </Row>
+    </Container>
+
+  </>;
 }
--- src/components/voucher/Voucher.js
+++ src/components/voucher/Voucher.js
@@ -1,49 +1,108 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetch } from '../../redux/VoucherSlice';
-import { Container, Grid, Card, CardContent, CardMedia, Typography, Button, CircularProgress } from '@mui/material';
+import { Container, Grid, Card, CardContent, CardMedia, Typography, Button, CircularProgress, Box, Divider, TextField, Input, useMediaQuery } from '@mui/material';
 import "./Voucher.css"
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from 'react-slick';
+
 export default function Voucher() {
     const dispatch = useDispatch();
     const vouchers = useSelector((state) => state.vouchers.vouchers);
-    const status = useSelector((state) => state.vouchers.status);
-    const error = useSelector((state) => state.vouchers.error);
+    const [buttonTexts, setButtonTexts] = useState({});
+    const copyInputRefs = useRef({});
+
+    const copyIt = (index) => {
+        const copyInput = copyInputRefs.current[index];
+        if (copyInput) {
+            copyInput.select();
+            document.execCommand('copy');
+
+            setButtonTexts((prev) => ({ ...prev, [index]: 'ĐÃ SAO CHÉP' }));
+
+            setTimeout(() => {
+                setButtonTexts((prev) => ({ ...prev, [index]: 'SAO CHÉP' }));
+            }, 15000);
+        }
+    };
 
     useEffect(() => {
         dispatch(fetch());
     }, [dispatch]);
 
+    const isMdUp = useMediaQuery('(min-width:960px)');
+    const isSmUp = useMediaQuery('(min-width:600px)');
+
+    const slidesToShow = isMdUp ? 3 : isSmUp ? 2 : 1;
+
+    const carouselSettings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: slidesToShow,
+        slidesToScroll: slidesToShow,
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+    };
+
     return (
-        <Container>
-            <Typography variant="h4" align="center" gutterBottom>
-                Danh sách sản phẩm
-            </Typography>
-            {status === "Đợi xíu nha" && <Typography>Loading...</Typography>}
-            {status === "Lỗi" && <Typography color="error">Error: {error}</Typography>}
-            <Grid container spacing={3}>
-                {vouchers.map((item, index) => (
-                    <Grid item xs={12} sm={6} md={4} key={index}>
-                        <Card className="voucher-card">
-                            <CardContent className="voucher-content">
-                                <div className="voucher-header">
-                                    <Typography variant="p">Enjoy Your Gift</Typography>
-                                </div>
-                                <div className="voucher-body">
-                                    <Typography variant="h4">50% OFF</Typography>
-                                    <Typography variant="h5">Coupon</Typography>
-                                    <Typography variant="body2">Valid until May, 2023</Typography>
-                                </div>
-                                <div className="voucher-footer">
-                                    <Typography variant="h6">87878521112</Typography>
-                                </div>
+        <Slider {...carouselSettings}>
+            {vouchers.slice(2).map((item, index) => (
+                <Box key={index} sx={{ padding: "2px", display: 'flex', justifyContent: 'center' }}>
+                    <Card style={{
+                        width: '100%',
+                        maxWidth: '90%',
+                        margin: '0 10px',
+                        padding: '10px',
+                        cursor: 'pointer',
+                        transition: 'transform 0.3s',
+                        borderRadius: '7px',
+                        backgroundColor: '#fff',
+                        boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.6)',
+                        '&:hover': { transform: 'scale(1.05)', boxShadow: '0px 8px 16px rgba(0, 0, 0, 0.2)' },
+                        '&:active': { boxShadow: '0px 12px 24px rgba(0, 0, 0, 0.3)' },
+                    }}>
+                        <Box style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+                            <Box style={{ marginBottom: '10px' }}>
+                                <img
+                                    src="https://i.pinimg.com/originals/c7/84/67/c78467db9ff497393cb548a48f02d451.png"
+                                    alt="Voucher"
+                                    width="70"
+                                    height="50"
+                                />
+                            </Box>
+                            <Divider orientation="vertical" flexItem style={{ margin: '0 10px' }} />
+                            <CardContent style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }}>
+                                <Typography variant="h4">Voucher</Typography>
+                                <Typography variant="body2">{item.name}</Typography>
+                                <Typography variant="h5">
+                                    {item.value}%<span> Coupon</span>
+                                </Typography>
                             </CardContent>
-                            <Button variant="contained" color="primary" className="use-button">
-                                Use Now
+                        </Box>
+                        <Box style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop: '10px' }}>
+                            <Input
+                                id={`copyvalue-${index + 2}`}
+                                inputRef={(el) => (copyInputRefs.current[index + 2] = el)}
+                                value={item.value_name}
+                                InputProps={{ readOnly: true }}
+                                variant="outlined"
+                                size="small"
+                                style={{ border: 'none', marginRight: '10px' }}
+                            />
+                            <Button
+                                variant="contained"
+                                onClick={() => copyIt(index + 2)}
+                                className="copybtn"
+                            >
+                                {buttonTexts[index + 2] || 'SAO CHÉP'}
                             </Button>
-                        </Card>
-                    </Grid>
-                ))}
-            </Grid>
-        </Container>
+                        </Box>
+                    </Card>
+                </Box>
+            ))}
+        </Slider>
     );
 }
